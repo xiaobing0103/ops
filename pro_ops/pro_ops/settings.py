735f1cd7f61ee1af1ca4c736d1e98acd322dba48
xiaobing0103 <2239856056@qq.com>
2020-12-23 09:20:35 +0800

    修改数据库连接设置

--- pro_ops/pro_ops/settings.py
+++ pro_ops/pro_ops/settings.py
@@ -76,18 +76,18 @@ WSGI_APPLICATION = 'pro_ops.wsgi.application'
 
 DATABASES = {
     'default': {
-    #     'ENGINE':'django.db.backends.mysql',
-	# 'NAME':'ops',
-	# 'USER':'ops',
-	# 'PASSWORD':'ops',
-	# 'HOST':'127.0.0.1',
-	# 'PORT':'3306',
-    #     'OPTIONS':{
-    #         'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"',
-    #         'charset':'utf8mb4'
-    #     }
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+         'ENGINE':'django.db.backends.mysql',
+	 'NAME':'ops',
+	 'USER':'ops',
+	 'PASSWORD':'ops',
+	 'HOST':'127.0.0.1',
+	 'PORT':'3306',
+         'OPTIONS':{
+             'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"',
+             'charset':'utf8mb4'
+         }
+#       'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
